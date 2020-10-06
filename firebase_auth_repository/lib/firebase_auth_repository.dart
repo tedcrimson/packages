5880773334334c585913c72599a27788775e5704
Ted <tedomakharadze95@gmail.com>
2020-10-06 22:57:11 +0400

    added firebase auth

---
+++ firebase_auth_repository/lib/firebase_auth_repository.dart
@@ -0,0 +1,3 @@
+library firebase_auth_repository;
+
+export './firebase_auth_repository.dart';
---
+++ firebase_auth_repository/lib/src/firebase_auth_repository.dart
@@ -0,0 +1,64 @@
+import 'dart:async';
+
+import 'package:authentication_repository/authentication_repository.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:meta/meta.dart';
+
+class FirebaseAuthenticationRepository<T> extends AuthenticationRepository<User, T> {
+  FirebaseAuthenticationRepository({
+    FirebaseAuth firebaseAuth,
+  })  : _firebaseAuth = firebaseAuth ?? FirebaseAuth.instance,
+        super() {
+    _userSubscription = userStream.listen((event) {
+      fireUser = event;
+    });
+  }
+
+  final FirebaseAuth _firebaseAuth;
+
+  User fireUser;
+  StreamSubscription<User> _userSubscription;
+
+  @override
+  Stream<User> get userStream {
+    return _firebaseAuth.authStateChanges();
+  }
+
+  String get userId => fireUser.uid;
+
+  @override
+  void dispose() {
+    _userSubscription.cancel();
+    super.dispose();
+  }
+
+  Future signUp({@required List fields});
+
+  @override
+  Future<void> logInWithEmailAndPassword({
+    @required String email,
+    @required String password,
+  }) async {
+    assert(email != null && password != null);
+    try {
+      await _firebaseAuth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      await super.logInWithEmailAndPassword(email: email, password: password);
+    } on Exception {
+      throw LogInWithEmailAndPasswordFailure();
+    }
+  }
+
+  @override
+  Future<void> logOut() async {
+    try {
+      await Future.wait([
+        _firebaseAuth.signOut(),
+      ]);
+    } on Exception {
+      throw LogOutFailure();
+    }
+  }
+}
