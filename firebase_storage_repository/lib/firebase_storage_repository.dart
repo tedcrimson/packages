cbb38620becf9a57f2c98e2022803648562fc543
Ted <tedomakharadze95@gmail.com>
2020-10-06 22:34:43 +0400

    fx

--- firebase_storage_repository/lib/firebase_storage_repository.dart
+++ firebase_storage_repository/lib/firebase_storage_repository.dart
@@ -1,7 +1,3 @@
 library firebase_storage_repository;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export './src/firebase_storage_repository.dart';
