c7fc1bebee586aa1a98d538124499b8e8d2ecb01
Ted <tedomakharadze95@gmail.com>
2020-10-14 17:28:39 +0400

    fixxxx

--- authentication_repository/lib/src/forms/string_form.dart
+++ authentication_repository/lib/src/forms/string_form.dart
@@ -4,7 +4,7 @@ class StringFieldForm extends FieldForm<String> {
   const StringFieldForm.dirty({String value = '', bool requiredField = false}) : super.dirty(value, requiredField);
 
   const StringFieldForm.pure(bool requiredField) : super.pure(requiredField);
-  factory StringFieldForm.constructor(String value, bool requiredField) =>
+  static constructor(String value, bool requiredField) =>
       StringFieldForm.dirty(value: value, requiredField: requiredField);
 
   @override
